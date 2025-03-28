1dc2cbcd9fe995360d880bff87e7ec1412eb825b
h356009 <h356009@stud-u.szeged.hu>
2025-03-28 17:05:17 +0100

    Flights - teszt.json és szűrő tesztelése

--- src/app/components/flights/flights.component.ts
+++ src/app/components/flights/flights.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { FilterPipe } from '../../pipes/filter.pipe';
+import flightsData from '../../../assets/teszt.json';
 
 @Component({
   selector: 'app-flights',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule, FilterPipe],
   templateUrl: './flights.component.html',
   styleUrl: './flights.component.scss',
 })
-export class FlightsComponent {}
+export class FlightsComponent {
+  flights: any[] = flightsData;
+}
