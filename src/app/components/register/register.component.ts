81c732d2524e82a84547921a95f191da3219c8de
sqzorange <fintus32@gmail.com>
2025-05-14 20:16:17 +0200

    register designolása

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -14,6 +14,7 @@ import {
   MatLabel,
 } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-register',
@@ -24,6 +25,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatButtonModule,
     ReactiveFormsModule,
     NavbarComponent,
+    MatCardModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
