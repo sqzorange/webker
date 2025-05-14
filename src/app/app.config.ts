b4c3bdea9476ee4399d3c35a1d5709afa19390fb
sqzorange <fintus32@gmail.com>
2025-05-14 19:55:46 +0200

    mini commit: Appró javítások és második deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,20 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "flightbuddy", appId: "1:923789910530:web:17b26a10e753072addfd1b", storageBucket: "flightbuddy.firebasestorage.app", apiKey: "AIzaSyBf75Zx2pgRAMRLkHhRSLg9dC7jZAgZudQ", authDomain: "flightbuddy-ce655.firebaseapp.com", messagingSenderId: "923789910530" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'flightbuddy',
+        appId: '1:923789910530:web:17b26a10e753072addfd1b',
+        storageBucket: 'flightbuddy.firebasestorage.app',
+        apiKey: 'AIzaSyBf75Zx2pgRAMRLkHhRSLg9dC7jZAgZudQ',
+        authDomain: 'flightbuddy-ce655.firebaseapp.com',
+        messagingSenderId: '923789910530',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
---
+++ src/app/components/flights/flight.ts
@@ -0,0 +1,7 @@
+export interface Flight {
+  id?: number;
+  departure: string;
+  arrival: string;
+  date: string;
+  classType: string;
+}
